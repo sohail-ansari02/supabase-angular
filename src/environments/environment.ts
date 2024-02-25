87b2f8d04ff4c7c990a6cfa2b2c3177148650000
sohail-ansari02 <syansari02@gmail.com>
2024-02-25 14:29:55 +0530

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://wjbyqibjclmscmssgwpe.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqYnlxaWJqY2xtc2Ntc3Nnd3BlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg4NTAwNzQsImV4cCI6MjAyNDQyNjA3NH0.8ZT4JmRv8us5khPaD7lhIOKuj3ShpWsISIbA0Z1SsnA', // anone key
+};
