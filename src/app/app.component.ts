cd40c77ca00619b993e1af4030722d687efa5b11
sohail-ansari02 <syansari02@gmail.com>
2024-02-25 14:29:30 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { SupabaseService } from './supabase.service'
+import { AuthComponent } from './auth/auth.component';
+import { AccountComponent } from './account/account.component';
+import { Session } from '@supabase/supabase-js';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [AuthComponent, AccountComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'supabase-angular';
+export class AppComponent implements OnInit {
+  title = 'angular-user-management'
+
+  session = this.supabase.session as any;
+
+  constructor(private readonly supabase: SupabaseService) {}
+
+  ngOnInit() {
+    this.supabase.authChanges((_, session) => (this.session = session))
+  }
 }
